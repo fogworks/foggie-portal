6874c90d6abdc8d326b81dfded42231e1a6a3cf2
zk <kai.zhang@megaops.net>
2023-06-09 15:29:59 +0800

    mod package

--- src/background.js
+++ src/background.js
@@ -9,7 +9,7 @@ import {
   dialog,
 } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
-import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
+// import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
 const isDevelopment = process.env.NODE_ENV !== "production";
 const path = require("path");
 const cp = require("child_process");
@@ -17,32 +17,178 @@ const { spawn } = require("child_process");
 const os = require("os");
 const fs = require("fs");
 const { initDownload } = require("./download.js");
+const express = require("express");
+const app1 = express();
+const checkMacOS = () => process.platform === "darwin";
+
+const net = require("net");
+
 let spawnObject = {
   childSpawn: null,
   childSpawn1: null,
 };
 let childProcess = null;
 let npmInstall = null;
-// let childSpawn;
-// let childSpawn1;
-// let childSpawn;
-// const checkMacOS = () => process.platform === 'darwin';
-// console.log('++++++++checkMacOS', checkMacOS())
-// console.log('++++++++__dirname', __dirname)
-// if (checkMacOS()) {
-//   // childSpawn = cp.spawn(`node ${path.join(__dirname, "bundled/foggie-node/server.js")}`);
-//   childSpawn = cp.spawn("node",["./foggie-node/server.js"], {
-//     shell:false,
-//     stdio: ['inherit', 'inherit', 'inherit','ipc']
-//   });
-// } else {
-//   // cp.spawn(path.join(__dirname, './winResources/xxx'));
-// }
+let user_path = os.homedir();
+let workingDir = null;
+let nodeProcess = null;
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: "app", privileges: { secure: true, standard: true } },
 ]);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  if (npmInstall) {
+    npmInstall.kill();
+    npmInstall = null;
+  }
+  if (spawnObject.childSpawn) {
+    spawnObject.childSpawn.kill();
+    spawnObject.childSpawn = null;
+  }
+  if (childProcess) {
+    childProcess.kill();
+    childProcess = null;
+  }
+  if (spawnObject.foggieProcess) {
+    spawnObject.foggieProcess.kill();
+    spawnObject.foggieProcess = null;
+  }
+  app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on("ready", async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    try {
+      // await installExtension(VUEJS3_DEVTOOLS);
+    } catch (e) {
+      console.error("Vue Devtools failed to install:", e.toString());
+    }
+  }
+  createWindow();
+});
+
+function isPortInUse(port) {
+  return new Promise((resolve, reject) => {
+    const server = net
+      .createServer()
+      .once("error", (err) => {
+        if (err.code === "EADDRINUSE") {
+          resolve(true); // 端口被占用
+        } else {
+          reject(err); // 其他错误
+        }
+      })
+      .once("listening", () => {
+        server.close();
+        resolve(false); // 端口未被占用
+      })
+      .listen(port);
+  });
+}
+
+function closePort(port) {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+
+    server.once("error", (err) => {
+      dialog.showErrorBox("close----error", err);
+      reject(err);
+      server.close();
+    });
+
+    server.once("close", () => {
+      dialog.showErrorBox("close----success", "");
+      resolve();
+    });
+
+    server.listen(port, "127.0.0.1", () => {
+      dialog.showErrorBox("close----listen", "");
+      server.close();
+    });
+  });
+}
+
+function initNodeProces() {
+  workingDir = `${__dirname}/foggie-node`;
+
+  process.chdir(workingDir);
+
+  nodeProcess = spawn("node", ["--experimental-fetch", "server.js"]);
+
+  nodeProcess.stdout.on("data", (data) => {
+    // dialog.showErrorBox("stdout", data.toString());
+  });
+
+  nodeProcess.stderr.on("data", (data) => {
+    // dialog.showErrorBox("stderr", data.toString());
+  });
+
+  nodeProcess.on("exit", (code) => {
+    dialog.showErrorBox("exit", `Child process exited with code ${code}`);
+    if (code === 0) {
+      initNodeProces();
+    }
+  });
+}
+
+function initFoggieProcess() {
+  spawnObject.foggieProcess = spawn(__dirname + "/foggie.exe", ["node"], {
+    detached: true,
+    env: {
+      PROX_LOG: `${user_path}/AppData/Roaming/foggie/Logs/foggie.log`,
+      // PROX_CONFIG: `${user_path}/Library/Application Support/foggie/config.json`
+      PROX_CONFIG: `${__dirname}/config.json`,
+    },
+  });
+
+  spawnObject.foggieProcess.stdout.on("data", (d) => {
+    dialog.showErrorBox("stdout", d.toString());
+  });
+
+  spawnObject.foggieProcess.stderr.on("data", (d) => {
+    dialog.showErrorBox("stderr", d.toString());
+  });
+
+  // spawnObject.foggieProcess.unref();
+
+  spawnObject.foggieProcess.on("exit", (code, signal) => {
+    dialog.showErrorBox("exit", `code: ${code}, signal: ${signal}`);
+    if (code === 0) {
+      initFoggieProcess();
+    }
+  });
+}
+
+function checkMemory() {
+  const totalMemory = os.totalmem(); // 获取系统总内存，单位为字节
+  const totalMemoryGB = totalMemory / (1024 * 1024 * 1024); // 将总内存转换为GB
+
+  if (totalMemoryGB < 8) {
+    console.log("系统内存小于8GB");
+    dialog.showMessageBox({
+      type: "info",
+      title: "提示",
+      message: "系统内存小于8GB，建议您升级内存",
+      buttons: ["确定"],
+    });
+  } else {
+    console.log("系统内存大于等于8GB");
+    dialog.showMessageBox({
+      type: "info",
+      title: "提示",
+      message: "系统内存d大于等于8GB",
+      buttons: ["确定"],
+    });
+  }
+}
 
 async function createWindow() {
   // Create the browser window.
@@ -64,10 +210,6 @@ async function createWindow() {
     // fullscreen: true,
     backgroundColor: "#fff",
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      // nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      // contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: false,
       nodeIntegration: true,
       nodeIntegrationInWorker: true,
@@ -79,15 +221,47 @@ async function createWindow() {
   });
 
   if (!isDevelopment) {
-    if (checkMacOS()) {
-      closePort(3000)
-        .then(() => {
-          // console.log('端口关闭成功');
-        })
-        .catch((error) => {
-          // console.error('发生错误', error);
-        });
+    // 使用示例
+    checkMemory();
+    isPortInUse(3000)
+      .then((inUse) => {
+        if (inUse) {
+          dialog.showErrorBox("foggie-node", "端口被占用");
+          dialog
+            .showMessageBox({
+              type: "question",
+              title: "确认",
+              message: "3000端口被占用，是否关闭？",
+              buttons: ["确定", "取消"],
+            })
+            .then((result) => {
+              if (result.response === 0) {
+                // 用户点击了确定按钮
+                // 执行操作
+                closePort(3000)
+                  .then(() => {
+                    // console.log('端口关闭成功');
+                  })
+                  .catch((error) => {
+                    // console.error('发生错误', error);
+                  });
+              } else {
+                // 用户点击了取消按钮
+                // 取消操作
+                return;
+              }
+            });
 
+          // console.log('端口被占用');
+        } else {
+          dialog.showErrorBox("foggie-node", "端口未被占用");
+          // console.log('端口未被占用');
+        }
+      })
+      .catch((err) => {
+        console.error("检查端口时出错:", err);
+      });
+    if (checkMacOS()) {
       closePort(8007)
         .then(() => {
           // console.log('端口关闭成功');
@@ -112,8 +286,6 @@ async function createWindow() {
           // dialog.showErrorBox("chmod", stderr);
         }
       );
-      // fs.chmod(`${__dirname}/foggie`, 1)
-      // fs.chmod(`${__dirname}/config.json`, 1)
 
       fs.readFile(
         path.join(__dirname, "./config.json"),
@@ -167,80 +339,16 @@ async function createWindow() {
         // dialog.showErrorBox("exit", `code: ${code}, signal: ${signal}`);
       });
     } else if (process.platform === "win32") {
-      let user_path = os.homedir();
-
-      const workingDir = `${__dirname}/foggie-node`;
-
-      process.chdir(workingDir); // 切换到指定目录
-
-      const childProcess = spawn("node", ["--experimental-fetch", "server.js"]);
-
-      childProcess.stdout.on("data", (data) => {
-        // dialog.showErrorBox("stdout", data.toString());
-      });
-
-      childProcess.stderr.on("data", (data) => {
-        // dialog.showErrorBox("stderr", data.toString());
-      });
-
-      childProcess.on("close", (code) => {
-        dialog.showErrorBox("close", `Child process exited with code ${code}`);
-      });
-
-      // const npmPath = path.join(process.env.APP_PATH, 'node_modules', '.bin', 'npm.cmd');
-      // const childProcess = spawn(npmPath, ["run", "serve"]);
-
-      // childProcess.stdout.on("data", (data) => {
-      //   console.log(data.toString());
-      // });
-
-      // childProcess.stderr.on("data", (data) => {
-      //   console.error(data.toString());
-      // });
-
-      // childProcess.on("close", (code) => {
-      //   console.log(`Child process exited with code ${code}`);
-      // });
-
-      // spawnObject.childSpawn = spawn(
-      //   `cd ${__dirname}/foggie-node && npm run serve`,
-      //   (err, stdout, stderr) => {
-      //     dialog.showErrorBox("foggie-node", stderr);
-      //   }
-      // );
-
-      // spawnObject.childSpawn1 = spawn(
-      //   `chmod +x ${__dirname}/foggie`,
-      //   (err, stdout, stderr) => {
-      //     dialog.showErrorBox("chmod", stderr);
-      //   }
-      // );
-
-      // spawnObject.childSpawn1 = spawn("chmod", ["+x", `${__dirname}/foggie.exe`]);
-
-      // spawnObject.childSpawn1.stdout.on("data", (data) => {
-      //   console.log(data.toString());
-      // });
-
-      // spawnObject.childSpawn1.stderr.on("data", (data) => {
-      //   console.error(data.toString());
-      // });
-
-      // spawnObject.childSpawn1.on("close", (code) => {
-      //   console.log(`Child process exited with code ${code}`);
-      // });
+      initNodeProces();
 
       const filePath = path.resolve(`${__dirname}/foggie.exe`); // Replace with the actual path to the file
-      fs.chmod(filePath, 0o755, err => {
+      fs.chmod(filePath, 0o755, (err) => {
         if (err) {
-          console.error('Error occurred while changing file permissions:', err);
+          console.error("Error occurred while changing file permissions:", err);
         } else {
-          console.log('File permissions changed successfully.');
+          initFoggieProcess();
         }
       });
-
-      // fs.chmod(`${__dirname}/foggie`, 1)
-      // fs.chmod(`${__dirname}/config.json`, 1)
 
       fs.readFile(
         path.join(__dirname, "./config.json"),
@@ -274,30 +382,6 @@ async function createWindow() {
           );
         }
       );
-
-      // const childProcess = cp.spawn(__dirname + "/mac-arm64/foggie-portal.app/Contents/Resources/app/foggie", ['node'], {
-      childProcess = spawn(__dirname + "/foggie.exe", ["node"], {
-        detached: true,
-        env: {
-          PROX_LOG: `${user_path}/AppData/Roaming/foggie/Logs/foggie.log`,
-          // PROX_CONFIG: `${user_path}/Library/Application Support/foggie/config.json`
-          PROX_CONFIG: `${__dirname}/config.json`,
-        },
-      });
-
-      childProcess.stdout.on("data", (d) => {
-        dialog.showErrorBox("stdout", d.toString());
-      });
-
-      childProcess.stderr.on("data", (d) => {
-        dialog.showErrorBox("stderr", d.toString());
-      });
-
-      childProcess.unref();
-
-      childProcess.on("exit", (code, signal) => {
-        dialog.showErrorBox("exit", `code: ${code}, signal: ${signal}`);
-      });
     }
   }
 
@@ -312,100 +396,6 @@ async function createWindow() {
   }
   initDownload(win);
 }
-
-// Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  // if (process.platform !== "darwin") {
-  // app.quit();
-  // }
-  // dialog.showErrorBox("close", spawnObject.childSpawn);
-  // dialog.showErrorBox("close1", spawnObject.childSpawn1);
-
-  // closePort(3000)
-  //   .then(() => {
-  //     dialog.showErrorBox("close----3000", "3000");
-  //     // console.log('端口关闭成功');
-  //   })
-  //   .catch((error) => {
-  //     dialog.showErrorBox("close---3000-----error", error);
-  //     // console.error('发生错误', error);
-  //   });
-
-  // closePort(8007)
-  //   .then(() => {
-  //     dialog.showErrorBox("close---8007", "8007");
-  //     // console.log('端口关闭成功');
-  //   })
-  //   .catch((error) => {
-  //     dialog.showErrorBox("close----error------8007", error);
-  //     // console.error('发生错误', error);
-  //   });
-  if (npmInstall) {
-    npmInstall.kill();
-    npmInstall = null;
-  }
-  if (spawnObject.childSpawn) {
-    spawnObject.childSpawn.kill();
-    spawnObject.childSpawn = null;
-  }
-  if (childProcess) {
-    childProcess.kill();
-    childProcess = null;
-  }
-  app.quit();
-});
-
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-
-const express = require("express");
-const app1 = express();
-const checkMacOS = () => process.platform === "darwin";
-
-const net = require("net");
-
-function closePort(port) {
-  return new Promise((resolve, reject) => {
-    const server = net.createServer();
-
-    server.once("error", (err) => {
-      dialog.showErrorBox("close----error", error);
-      reject(err); // 发生错误
-      server.close();
-    });
-
-    server.once("close", () => {
-      dialog.showErrorBox("close----success", "");
-      resolve(); // 端口关闭成功
-    });
-
-    server.listen(port, "127.0.0.1", () => {
-      dialog.showErrorBox("close----listen", "");
-      server.close();
-    });
-  });
-}
-
-app.on("ready", async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      // await installExtension(VUEJS3_DEVTOOLS);
-    } catch (e) {
-      console.error("Vue Devtools failed to install:", e.toString());
-    }
-  }
-  createWindow();
-});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
